c3356ac40eea1d60a6322591b03b662200063cff
samwijayag <sam.wiajayg@gmail.com>
2019-01-06 21:27:11 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ToDoItems from './Components/ToDoItems'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+const App = () =>
+{
+	return (
+		<div className='todo-list'>
+			<ToDoItems />
+			<ToDoItems />
+			<ToDoItems />
+			<ToDoItems />
+		</div>
+	)
 }
 
-export default App;
+
+export default App
---
+++ src/Components/ToDoItems.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const ToDoItems =() => {
+	return (
+		<div className='todo-item'>
+			<input type= 'checkbox' />
+			<p>Place holder text</p>
+		</div>
+	)
+}
+
+
+
+
+export default ToDoItems
