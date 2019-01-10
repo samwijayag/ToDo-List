c5095104628d082e8753bcfa7bbace5b2cff6ab6
samwijayag <sam.wiajayg@gmail.com>
2019-01-09 16:57:38 -0800

    added onchange

--- src/Components/ToDoItems.js
+++ src/Components/ToDoItems.js
@@ -4,7 +4,10 @@ import React from 'react'
 const ToDoItems =(props) => {
 	return (
 		<div className='todo-item'>
-			<input type= 'checkbox' checked= {props.item.completed}/>
+			<input type= 'checkbox' 
+			checked= {props.item.completed}
+			onChange = {() => console.log("changed")}
+			/>
 			<p>{props.item.text}</p>
 		</div>
 	)
